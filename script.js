13bda0b45d397d06337ad4492c701a02329a60e2
Ömer Akgül <oemer.akguel@gmx.de>
2024-12-21 10:04:17 +0100

    added the fetchAlbumData function limited the amout of titles (100) to 10 with link parameters

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-console.log("Script loaded");
-console.log("Das ist der zweite Aufruf");
+//console.log("Script loaded");
+//console.log("Das ist der zweite Aufruf");
 const userlist = document.getElementById('user-list');
 const imageList = document.getElementById('image-list');
+const albumList = document.getElementById('album-list');
 
 
 // Define an array for users
 let users = []; 
 let images = []; // to be filled with images from api endpoint https://jsonplaceholder.typicode.com/photos
+let albums = [];
 
 // define async function to fetch users data
 async function fetchUserData(){
@@ -16,7 +18,7 @@ async function fetchUserData(){
         usersData = await response.json();
         // console.log(usersData);
         users = usersData;
-        console.log(users);
+        //console.log(users);
         renderUsers();
         
     } catch (error) {
@@ -39,14 +41,27 @@ async function fetchImageData() {
     
 }
 
+async function fetchAlbumData() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/albums?_limit=10');
+        albumData = await response.json();
+        albums = albumData;
+        // console.log(images);
+        renderAlbums();
+    } catch (error) {
+        console.log("Wir bekommen beim Fetching der Album API folgenden Fehler: ", error)
+    }
+    
+}
 fetchUserData();
 fetchImageData();
+fetchAlbumData();
 
 function renderUsers(){
     users.forEach((user) => {
         const userItem = document.createElement('li');
         userItem.innerHTML = user.name;
-        // console.log(user);
+        //console.log(user);
 
         userlist.appendChild(userItem);
     });
@@ -55,9 +70,20 @@ function renderUsers(){
 function renderImages(){
     images.forEach((image) => {
         const imageItem = document.createElement('img');
-        console.log(imageItem);
+        //console.log(imageItem);
         imageItem.setAttribute('src', image.url)
+        imageItem.setAttribute('width', '200')
         imageList.appendChild(imageItem)
+
+    });
+}
+
+function renderAlbums(){
+    albums.forEach((album) => {
+        const albumItem = document.createElement('li');
+        albumItem.innerHTML = album.id + '. ' + album.title
+        console.log(albumItem.innerHTML)
+        albumList.appendChild(albumItem)
 
     });
 }
